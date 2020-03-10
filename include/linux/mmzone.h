05bcc7bc2af175804f335f49e43afa89afe5b940
Greg Kroah-Hartman <gregkh@google.com>
2020-03-10 18:42:05 +0100

    ANDROID: GKI: mm: add Android ABI padding to some structures
    Try to mitigate potential future driver core api changes by adding a
    padding to stuct vm_area_struct and struct zone.
    
    Based on a patch from Michal Marek <mmarek@suse.cz> from the SLES kernel
    
    Bug: 151154716
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I81702aa833f419928e0e32e9609722b98592c171

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -18,6 +18,7 @@
 #include <linux/pageblock-flags.h>
 #include <linux/page-flags-layout.h>
 #include <linux/atomic.h>
+#include <linux/android_kabi.h>
 #include <asm/page.h>
 
 #include <linux/android_kabi.h>
@@ -521,6 +522,11 @@ struct zone {
 	/* Zone statistics */
 	atomic_long_t		vm_stat[NR_VM_ZONE_STAT_ITEMS];
 	atomic_long_t		vm_numa_stat[NR_VM_NUMA_STAT_ITEMS];
+
+	ANDROID_KABI_RESERVE(1);
+	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
+	ANDROID_KABI_RESERVE(4);
 } ____cacheline_internodealigned_in_smp;
 
 enum pgdat_flags {
