679dae4df8f690faa5306a56d1d6ecd91da5b86d
Mizumo-prjkt <bluepearl394@gmail.com>
2025-12-28 11:17:09 +0800

    fix: include/net/genetlink.h & net/netlink/genetlink.c: Found the underlying issue, fixed the problem
    Follow up to 3899e84f6ec87bbe2f062f8595e2fa2f0f92a460
    where we finally figured out the cause of genetlink having issues
    where there were 'missing' member on the source.
    
    I would like to blame ACK for this and also git for slipping the changes
    through
    
    ACK Ref:
    https://android-review.googlesource.com/c/kernel/common/+/1370057
    Merge ID: b41585fc931611c43fda58e06d5b4eb6a93d9785
    
    Now, the issue of genetlink should be fixed.
    
    Signed-off-by: Mizumo-prjkt <bluepearl394@gmail.com>

--- include/net/genetlink.h
+++ include/net/genetlink.h
@@ -56,6 +56,8 @@ struct genl_family {
 	void			(*post_doit)(const struct genl_ops *ops,
 					     struct sk_buff *skb,
 					     struct genl_info *info);
+	int			(*mcast_bind)(struct net *net, int group);
+	void			(*mcast_unbind)(struct net *net, int group);
 	struct nlattr **	attrbuf;	/* private */
 	const struct genl_ops *	ops;
 	const struct genl_multicast_group *mcgrps;
